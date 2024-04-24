d4cd888b3a8ff03c13d770f939814ba97c3e298d
jon-oblander <jonoblander@pingidentity.com>
2024-04-24 12:31:00 -0700

    updates

--- davinci-oidc_sdk-ciam_passwordless_solution-pkg/terraform/outputs.tf
+++ davinci-oidc_sdk-ciam_passwordless_solution-pkg/terraform/outputs.tf
@@ -6,3 +6,11 @@
 output "pingone_envid" {
   value = pingone_environment.my_environment.id
 }
+
+output "pingone_domain" {
+  value = module.pingone_utils.pingone_domain_suffix
+}
+
+output "oauth_client_id" {
+  value = pingone_application.oidc_sdk_sample_app.id
+}
