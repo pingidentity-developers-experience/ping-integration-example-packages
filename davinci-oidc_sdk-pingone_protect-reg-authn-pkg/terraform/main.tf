4088d9acd76ad5416d8a965e0f432f012b3eec6f
Eric Anderson <ericanderson@pingidentity.com>
2024-09-06 09:57:45 -0600

    Updated DaVinci provider version in passworldless package #188180223

--- davinci-oidc_sdk-pingone_protect-reg-authn-pkg/terraform/main.tf
+++ davinci-oidc_sdk-pingone_protect-reg-authn-pkg/terraform/main.tf
@@ -59,6 +59,7 @@ provider "pingone" {
   client_id                    = var.worker_id
   client_secret                = var.worker_secret
   environment_id               = var.pingone_environment_id
+  region_code                  = var.region_code
 }
 
 ##############################################
