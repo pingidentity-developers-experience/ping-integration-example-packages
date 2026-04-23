b5dfb42681f6e574ebc275a93c3971e983f91b56
Eric Anderson <ericanderson@pingidentity.com>
2026-04-23 07:59:56 -0600

    Resolved a warning about agreement_en_now defaulting to now, explicitly defined that so intent is clear DOE-2236

--- davinci-oidc_sdk-ciam_passwordless_solution-pkg/terraform/versions.tf
+++ davinci-oidc_sdk-ciam_passwordless_solution-pkg/terraform/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     pingone = {
       source  = "pingidentity/pingone"
-      version = "1.18.0"
+      version = "1.19.0"
     }
     time = {
       source = "hashicorp/time"
